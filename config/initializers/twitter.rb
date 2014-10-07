08c2115af37d7cfd51e62e3aea38800bb9b49463
Prelang Builder <builder@prelang.com>
2014-10-07 11:41:57 +0100

    Executed 'bundle install' to fetch and install required Ruby Gems

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "itBM0Pi2d39iHw2kfQY5wJeGP"
+  config.consumer_secret     = "SesBIRpUgbYvWQWPGAUO20o6OXkLQBrgmULlIrwRCwl3aGd8Zx"
+  config.access_token        = "2831797387-yjdd5xMoNWAeY63Je02zX5mijFExSwAJTpUjBPd"
+  config.access_token_secret = "EBav2znYvo212Svea2VBeK21glj5YISXHeDqiYwZaoKXF"
+end
